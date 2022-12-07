892c6855899248fdfedf6be5fd2e0f4d11c445ed
Henrique Jose Alves Gonzaga <henriquealves87@hotmail.com>
2022-12-07 21:45:04 +0000

    forms Student created
    the form with name and student form was created

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField('name', max_length=200)
+    register = models.IntegerField('register number')
+
+    def __str__(self):
+        return f'name: {self.name}, register number: {self.register}'

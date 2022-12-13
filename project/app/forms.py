c87402d9aa7ae5faf3cbc2d4961112ede4adf4e7
Henrique Jose Alves Gonzaga <henriquealves87@hotmail.com>
2022-12-13 22:50:20 +0000

    grades forms created
    I made the relation between student and grades forms

--- project/app/forms.py
+++ project/app/forms.py
@@ -5,4 +5,10 @@ from .models import *
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['name', "id"]
+        fields = ['id', 'name']
+
+
+class GradeForm(forms.ModelForm):
+    class Meta:
+        model = Grades
+        fields = ['Student', '1st Grade', '2nd Grade', '3rd Grade']
--- project/app/models.py
+++ project/app/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 
 class Student(models.Model):
-    id = models.BigAutoField('id', primary_key=True)
-    name = models.CharField('name', max_length=200)
+    id = models.SmallAutoField('id', primary_key=True)
+    name = models.CharField('name', max_length=500)
 
     def __str__(self):
-        return f"name: {self.name}, id: {self.id}"
+        return f"name: {self.name}"
+
+
+class Grades(models.Model):
+    id = models.SmallAutoField(primary_key=True)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    g1 = models.PositiveSmallIntegerField('1st Grade')
+    g2 = models.PositiveSmallIntegerField('2nd Grade')
+    g3 = models.PositiveSmallIntegerField('3rd Grade')
+
+    def __str__(self):
+        return f"student: {self.student}, 1st Grade: {self.g1}, 2nd Grade: {self.g2}, 3rd Grade: {self.g3}"
